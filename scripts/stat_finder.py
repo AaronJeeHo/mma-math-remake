7d6cf1fb2478b5f84df5486abaac5562f8a9c4e1
Aaron <aaron.jee.ho@gmail.com>
2020-09-04 00:23:06 -0700

    Striking data scraped

--- scripts/stat_finder.py
+++ scripts/stat_finder.py
@@ -28,21 +28,87 @@ def name_to_url(name):
     return None
 
 
+def frac_to_stats(stats):
+    """
+    Parse stats represented as fraction
+    :param list stats:
+    :return:
+    """
+    landed = 0
+    attempt = 0
+
+    for frac in stats:
+        split_vals = frac.split('/')
+        landed += int(split_vals[0])
+        attempt += int(split_vals[1])
+
+    return round((landed / attempt) * 100, 2)
+
+
+def percent_to_stats(df, col):
+    """
+    Return dataframe col of percentages as stats
+    :param DataFrame df:
+    :param str col:
+    :return:
+    """
+
+    return round(df[col].str.rstrip('%').astype('float').mean(), 2)
+
+
+
 def scrape_stats(link):
     split_url = link.split('_')
     stat_link = f"{split_url[0]}stats/_{split_url[1]}"
 
-    response = requests.get(stat_link)
-    src = response.content
+    try:
+        df_list = pd.read_html(stat_link)
 
-    soup = BeautifulSoup(src, 'lxml')
+        # Get striking stats
+        striking = df_list[0]
 
-    print(soup.prettify())
+        print(striking.columns)
+
+        body_s = list(striking.loc[(striking['SDBL/A'] != '-') &
+                                   (striking['SDBL/A'] != '0/0')]['SDBL/A'])
+
+        head_s = list(striking.loc[(striking['SDHL/A'] != '-') &
+                                   (striking['SDHL/A'] != '0/0')]['SDHL/A'])
+
+        leg_s = list(striking.loc[(striking['SDLL/A'] != '-') &
+                                  (striking['SDLL/A'] != '0/0')]['SDLL/A'])
+
+        t_sigs = striking.loc[(striking['SSL'] != '-') &
+                              (striking['SSA'] != '-') &
+                              (striking['SSA'] != '0')]
+
+        targets = striking.loc[((striking['%BODY'] != '0%') |
+                                (striking['%HEAD'] != '0%') |
+                                (striking['%LEG'] != '0%')) &
+                               (striking['%BODY'] != '-') &
+                               (striking['%HEAD'] != '-') &
+                               (striking['%LEG'] != '-')]
+
+
+
+
+
+
+
+
+
+
+
+    except ImportError:
+        print('Stats not found')
+        return None
 
 
 def main():
-    # fighter_url = name_to_url('Khabib Nurmagomedov')
-    # scrape_stats(fighter_url)
+    link = name_to_url('Khabib Nurmagomedov')
+    scrape_stats(link)
+
+
 
 
 if __name__ == '__main__':
