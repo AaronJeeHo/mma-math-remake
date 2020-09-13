550320e04f5719109e865af6e947b7756f51f982
Aaron <aaron.jee.ho@gmail.com>
2020-09-13 02:12:38 -0700

    Deleted Old Data, rebuilding database

--- scripts/path_finder.py
+++ scripts/path_finder.py
@@ -130,6 +130,78 @@ def make_graph(fighter_a, fighter_b):
         print('No path to victory found')
         return None
 
+def scrape_wins(link):
+    try:
+        split_url = link.split('_')
+        history_link = f"{split_url[0]}history/_{split_url[1]}"
+        df = (pd.read_html(history_link)[0])[['Opponent', 'Res.']]
+        return tuple(df.loc[df['Res.'] == 'W']['Opponent'])
+
+    except ImportError:
+        return None
+
+
+# def scrape_graph(db, fighter_a, fighter_b):
+#     """
+#     Creates a graph seeing if Fighter A can beat Fighter B
+#     :param str fighter_a: Name of fighter
+#     :param str fighter_b: Name of fighter
+#     :return: FighterGraph
+#     """
+#
+#     # if len(get_losses(name_to_file(fighter_b))) < 1:
+#     #     print('No path, fighter is undefeated')
+#     #     return None
+#
+#     if fighter_a == fighter_b:
+#         print(f'{fighter_a} can beat {fighter_b}')
+#         return None
+#
+#     fight_history = FightGraph()
+#     to_add = deque([fighter_a])
+#     fight_history.dist[fighter_a] = 0
+#     visited = {fighter_a}
+#
+#     print('Finding path...')
+#     while len(to_add) > 0:
+#         curr_fighter = to_add.popleft()
+#         fighter_wins = scrape_wins(name_to_url(db, curr_fighter))
+#
+#         if fighter_wins is None or len(fighter_wins) < 1:
+#             continue
+#         else:
+#             if fighter_b in fighter_wins:
+#                 fight_history.path_found = True
+#                 next_dist = int(fight_history.dist[curr_fighter]) + 1
+#
+#                 if next_dist < fight_history.shortest:
+#                     fight_history.prev[fighter_b] = curr_fighter
+#                     fight_history.dist[fighter_b] = next_dist
+#                     fight_history.shortest = next_dist
+#                     print(f"Path found with length {next_dist}")
+#             else:
+#                 fight_history.add_fighter(curr_fighter, fighter_wins)
+#                 next_dist = int(fight_history.dist[curr_fighter]) + 1
+#
+#                 if next_dist < fight_history.shortest - 1:
+#
+#                     for i in fighter_wins:
+#                         if i not in visited:
+#                             to_add.append(i)
+#                             visited.add(i)
+#                             fight_history.prev[i] = curr_fighter
+#                             fight_history.dist[i] = next_dist
+#
+#                         elif fight_history.dist[i] > next_dist:
+#                             fight_history.dist[i] = next_dist
+#                             fight_history.prev[i] = curr_fighter
+#
+#     if fight_history.path_found is True:
+#         return fight_history
+#     else:
+#         print('No path to victory found')
+#         return None
+
 
 def get_path(f_graph, fighter_a, fighter_b):
     path = []
@@ -143,6 +215,12 @@ def get_path(f_graph, fighter_a, fighter_b):
     path.reverse()
     return path
 
+
+# def mma_math(db, fighter_a, fighter_b):
+#     graph = scrape_graph(db, fighter_a, fighter_b)
+#     path = get_path(graph, fighter_a, fighter_b)
+#
+#     return path
 
 def mma_math(fighter_a, fighter_b):
     graph = make_graph(fighter_a, fighter_b)
@@ -159,9 +237,9 @@ def main():
 
     # print(name_to_file(name_db, f2))
 
-    # path = mma_math(f1, f2)
-    #
-    # print(' -> '.join(path))
+    path = mma_math(f1, f2)
+
+    print(' -> '.join(path))
 
 
 if __name__ == '__main__':
